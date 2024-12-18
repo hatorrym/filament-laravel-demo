4d1aa47fa74a20250698a0d93fc59e49c8b7fb0e
synv <synv@bsscommerce.com>
2024-12-18 13:52:40 +0700

    init

---
+++ bootstrap/providers.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    App\Providers\AppServiceProvider::class,
+    App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\Filament\SynvPanelProvider::class,
+    App\Providers\Filament\UsersynvPanelProvider::class,
+    App\Providers\FortifyServiceProvider::class,
+    App\Providers\JetstreamServiceProvider::class,
+];
